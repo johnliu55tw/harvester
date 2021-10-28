00a316d7da3572cb4e0550d93fa1fdffddcb8c28
John Liu <john.liu@suse.com>
2021-10-28 23:02:40 +0800

    Update NDM DaemonSet when settings changed
    If users change settings related to NDM, update the DaemonSet to ensure
    pods are reloaded.
    
    Currently only "AutoAddDiskPaths" setting is associated with NDM.

--- pkg/controller/master/setting/controller.go
+++ pkg/controller/master/setting/controller.go
@@ -3,11 +3,15 @@ package setting
 import (
 	"github.com/sirupsen/logrus"
 
+	appsv1 "github.com/rancher/wrangler/pkg/generated/controllers/apps/v1"
+
 	harvesterv1 "github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
 )
 
 // Handler updates the log level on setting changes
 type Handler struct {
+	dsClient appsv1.DaemonSetClient
+	dsCache  appsv1.DaemonSetCache
 }
 
 func (h *Handler) LogLevelOnChanged(key string, setting *harvesterv1.Setting) (*harvesterv1.Setting, error) {
@@ -22,5 +26,40 @@ func (h *Handler) LogLevelOnChanged(key string, setting *harvesterv1.Setting) (*
 
 	logrus.Infof("set log level to %s", level)
 	logrus.SetLevel(level)
+	return setting, nil
+}
+
+func (h *Handler) AutoAddDiskPathsOnChanged(key string, setting *harvesterv1.Setting) (*harvesterv1.Setting, error) {
+	if setting == nil || setting.DeletionTimestamp != nil || setting.Name != "auto-add-disk-paths" {
+		return setting, nil
+	}
+
+	logrus.Debug("AudoAddDiskPathOnChanged")
+	ds, err := h.dsCache.Get("harvester-system", "harvester-node-disk-manager")
+	if err != nil {
+		return setting, nil
+	}
+
+	dsCopy := ds.DeepCopy()
+
+	for i, cont := range dsCopy.Spec.Template.Spec.Containers {
+		if cont.Name == "harvester-node-disk-manager" {
+			for j, env := range cont.Env {
+				if env.Name == "NDM_AUTO_ADD_PATH" {
+					logrus.Infof("update NDM_AUTO_ADD_PATH to '%s'", setting.Value)
+					dsCopy.Spec.Template.Spec.Containers[i].Env[j].Value = setting.Value
+				}
+			}
+		}
+	}
+
+	logrus.Info(dsCopy.Spec.Template.Spec.Containers)
+
+	_, err = h.dsClient.Update(dsCopy)
+	if err != nil {
+		logrus.Errorf("unable to update NDM daemonset: %v", err)
+		return setting, err
+	}
+
 	return setting, nil
 }
--- pkg/controller/master/setting/register.go
+++ pkg/controller/master/setting/register.go
@@ -12,8 +12,15 @@ const (
 
 func Register(ctx context.Context, management *config.Management, options config.Options) error {
 	settings := management.HarvesterFactory.Harvesterhci().V1beta1().Setting()
-	controller := &Handler{}
+
+	var dsClient = management.AppsFactory.Apps().V1().DaemonSet()
+	var dsCache = dsClient.Cache()
+	controller := &Handler{
+		dsClient: dsClient,
+		dsCache:  dsCache,
+	}
 
 	settings.OnChange(ctx, controllerName, controller.LogLevelOnChanged)
+	settings.OnChange(ctx, controllerName, controller.AutoAddDiskPathsOnChanged)
 	return nil
 }
